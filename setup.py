52c58d7bedbd9f1fb92c8d366cda71765c4e738a
JiashengLai <33054127+JiashengLai@users.noreply.github.com>
2017-11-19 02:04:47 +0100

    Add files via upload
    setup.py uploaded

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+
+
+from setuptools import setup, find_packages
+
+
+setup (name = 'Poisson_Solver',
+       version='0.0.1',
+	   description='a poisson solver',
+	   long_description=readme,
+       url='https://github.com/JiashengLai/C_S_Poison-Solver.git',
+	   author='CS Group',
+	   license='LICENSE',
+	   packages=['Poisson_Solver'],
+	   )
